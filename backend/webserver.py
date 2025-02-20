804159ddfec75231e55d50887d88dbdb5de26211
Simonkho <simonkrh@gmail.com>
2025-02-20 21:21:49 +0100

    Update webserver.py

--- backend/webserver.py
+++ backend/webserver.py
@@ -1,11 +1,11 @@
 # Flask-server som serverer nettside
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder='../frontend/static')
 
 @app.route('/')
 def index():
-    return send_from_directory('static', 'index.html')
+    return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
