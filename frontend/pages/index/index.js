80f2540648ad650621b6bbbbf0e066eecb70e435
Simonkho <simonkrh@gmail.com>
2025-03-06 21:33:26 +0100

    Start at random image in slideshow

--- frontend/pages/index/index.js
+++ frontend/pages/index/index.js
@@ -6,7 +6,7 @@ fetch(`http://${serverIP}:5010/images`)
   .then(data => {
     images = data;
     if (images.length > 0) {
-      let index = 0;
+      let index = Math.floor(Math.random() * images.length);
 
       const img1 = document.getElementById("image1");
       const img2 = document.getElementById("image2");
